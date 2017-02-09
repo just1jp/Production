ae4c339574521a1d5ddb5def5d5dd146a69d6241
Kay Christensen <kaychristensen@gmail.com>
2017-02-09 14:21:37 -0800

    Configure default theme and colors

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -2,4 +2,14 @@
   * @class angular_module.myApp
   @description Main application module. All services, controllers, and config files are subordinate to this module.  
   */
-  angular.module('myApp', ['ngMaterial', 'ngRoute', 'firebase', 'ngGeolocation', 'ngMap']);
+  angular.module('myApp', ['ngMaterial', 'ngRoute', 'firebase', 'ngGeolocation', 'ngMap'])
+    .config(function($mdThemingProvider) {
+      $mdThemingProvider.theme('default')
+      .primaryPalette('indigo', {
+        'default': '500',
+        'hue-1': '50',
+        'hue-2': '100',
+        'hue-3': '300'
+      }) 
+      .accentPalette('blue-grey');
+    });
