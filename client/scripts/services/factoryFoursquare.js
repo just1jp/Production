4dd175ff72e1fbf1c822a6e488552d2238a1ca5d
Justin Mendelson <jmendelson22@gmail.com>
2017-02-08 20:17:28 -0800

    Radius calculation

--- client/scripts/services/factoryFoursquare.js
+++ client/scripts/services/factoryFoursquare.js
@@ -14,22 +14,20 @@ angular.module('myApp').factory('foursquare', function($http, databaseAndAuth) {
         snapshot.forEach(user => {
           var key = user.key;
           var userData = user.val();
-
-          console.log('key: ', key);
-          console.log('userData: ', userData.coordinates);
-
           userLocations.push(userData.coordinates);
         });
 
         console.log('all Data: ', userLocations);
 
-        this.calculateMidpoint(userLocations);
+        this.calculateCircle(userLocations);
       });
   }
 
-  factory.calculateMidpoint = function(arr) {
+  factory.calculateCircle = function(arr) {
     var totalLat = 0;
     var totalLon = 0;
+    // Add minimum radius constraint if all users are clustered together
+    var radius = 500;
     var denom = arr.length;
 
     arr.forEach(location => {
@@ -40,35 +38,58 @@ angular.module('myApp').factory('foursquare', function($http, databaseAndAuth) {
     var avgLat = totalLat / denom;
     var avgLon = totalLon / denom;
 
-    var latLon = [avgLat, avgLon];
+    var latLon = [avgLat, avgLon];    
+
+    arr.forEach(location => {
+
+      // Calculate distance between two coordinates
+      var lat1 = avgLat;
+      var lon1 = avgLon;
+      var lat2 = location.latitude;
+      var lon2 = location.longitude;
+      function deg2rad(deg) {
+        return deg * (Math.PI/180)
+      }
+      var R = 6371; // Radius of the earth in km
+      var dLat = deg2rad(lat2-lat1);  // deg2rad below
+      var dLon = deg2rad(lon2-lon1); 
+      var a = 
+        Math.sin(dLat/2) * Math.sin(dLat/2) +
+        Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+        Math.sin(dLon/2) * Math.sin(dLon/2); 
+      var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+      var distance = R * c * 1000; // Distance in m
+
+      // Update radius to largest distance between points
+      if (distance > radius) {
+        radius = distance;
+      }
+
+    });
 
     console.log('latLon: ', latLon);
+    console.log('radius: ', radius);
 
-    // this.calculateRadius(arr, latLon);
-  }
-
-  factory.calculateRadius = function(arr, midpoint) {
-
-    
-
-
+    this.getFoursquareData(avgLat, avgLon, radius);
   }
 
 
-  factory.getFoursquareData = function() {
+  factory.getFoursquareData = function(lat, long, radius) {
 
+    var latlon = lat + "," + long;
 
     $http.get('/api/foursquare', 
     {
       params: {
-        ll: "37.806515,-122.416934",
-        radius: 250,
+        ll: latlon,
+        radius: radius,
         llAcc: 20,
         limit: 10,
         section: 'food'
       }
     })
     .then( result => {
+      console.log('this is result data ', result.data.response);
       var places = result.data.response.groups[0].items;
       
       places.forEach(place => {
