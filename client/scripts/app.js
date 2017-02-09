6c12e20d4f3dcbfe572e0830c02a8ed5ea641cd9
Kay Christensen <kaychristensen@gmail.com>
2017-02-09 10:46:11 -0800

    Add ngMaterial as dependency

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -2,4 +2,4 @@
   * @class angular_module.myApp
   @description Main application module. All services, controllers, and config files are subordinate to this module.  
   */
-  angular.module('myApp', ['ngRoute', 'firebase', 'ngGeolocation', 'ngMap']);
+  angular.module('myApp', ['ngMaterial', 'ngRoute', 'firebase', 'ngGeolocation', 'ngMap']);
