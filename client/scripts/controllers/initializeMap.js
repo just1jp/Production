8f38a1706c5b3c94ddcdd08d2d23f2f546ed7eb0
Justin Mendelson <jmendelson22@gmail.com>
2017-02-10 14:48:01 -0800

    Merging conflicts with master

--- client/scripts/controllers/initializeMap.js
+++ client/scripts/controllers/initializeMap.js
@@ -5,10 +5,6 @@
 
 angular.module('myApp').controller('initializeMap', function($rootScope, $scope, databaseAndAuth, coordinateCalc, foursquare, NgMap) {
 
-  $scope.clickLocation = function(event, index) {
-    $scope.highlight = { selected: index };
-  }
-
   $scope.$on('user:updatedOrAdded', function(event, data) {
     $scope.userLocations[data[0]] = data[1];
     updateCenterPointAndRadius();
@@ -21,7 +17,7 @@ angular.module('myApp').controller('initializeMap', function($rootScope, $scope,
     });
     
     // console.log('foursquare array', $scope.foursquareLocations);
-    console.log('foursquare location latitude', $scope.foursquareLocations[0].venue.location.lat);
+    // console.log('foursquare location latitude', $scope.foursquareLocations[0].venue.location.lat);
     $scope.$apply();
   });
 
@@ -45,18 +41,66 @@ angular.module('myApp').controller('initializeMap', function($rootScope, $scope,
   }
 
   NgMap.getMap().then(function(map) {
+    renderLocationsonMap().then(function(foursquareLocations) {
+      foursquareLocations.forEach((location, index) => {
+        // Add a marker on the map for each foursquare location
+        setTimeout(function() {
+          var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(location.venue.location.lat, location.venue.location.lng),
+            map: map,
+            draggable: false,
+            animation: google.maps.Animation.DROP,
+            name: location.venue.name
+          });
+          var overlay = new CustomMarker(
+            new google.maps.LatLng(location.venue.location.lat, location.venue.location.lng), 
+            map,
+            {
+              marker_id: location.venue.name,
+              innerHTML: '<p class="location-label" ng-class="{ location_highlighted: $index == highlight.selected }">' + location.venue.name + '</p>'
+            }
+          );
+          marker.addListener('click', clickLocation);
+        }, index * 200);
+      })
+    })
+
+    // <----- NEED TO REPLACE THIS ----->
+    // <div ng-repeat="location in foursquareLocations track by $index">
+    //   <marker
+    //     on-click="clickLocation($index)"
+    //     name="{{location.venue.name}}"
+    //     position="{{location.venue.location.lat}}, {{location.venue.location.lng}}"
+    //     animation="DROP">
+    //   </marker>
+    //   <custom-marker
+    //     on-click="clickLocation($index)"
+    //     key="location-{{$index}}" 
+    //     position="{{location.venue.location.lat}}, {{location.venue.location.lng}}">
+    //     <p class="location-label" ng-class="{ location_highlighted: $index == highlight.selected }">{{location.venue.name}}</p>
+    //   </custom-marker>
+    // </div>
+
   });
 
-  function renderLocationsonMap() {
-    databaseAndAuth.database.ref('/foursquare_results').once('value').then(function(snapshot) {
-      $scope.foursquareLocations = [];
+  var clickLocation = function() {
+    console.log(this.name);
+    $scope.highlight = { selected: index };
+  }
+
+  // Create a promise that returns an array of foursquare locations currently in Firebase
+  var renderLocationsonMap = function() {
+    return databaseAndAuth.database.ref('/foursquare_results').once('value').then(function(snapshot) {
+      foursquareLocations = [];
       for (key in snapshot.val()) {
-        $scope.foursquareLocations.push(snapshot.val()[key]);
+        foursquareLocations.push(snapshot.val()[key]);
       }
+      return foursquareLocations;
     });
   }
 
-  function updateCenterPointAndRadius() {
+  // Recalculate the search coordinates for the map
+  var updateCenterPointAndRadius = function() {
     coordinateCalc.getUserLocationData().then(function(coordinates) {
       circleData = coordinateCalc.calculateCircle(coordinates);
       
@@ -72,10 +116,82 @@ angular.module('myApp').controller('initializeMap', function($rootScope, $scope,
 
       // renderLocationsonMap();
 
-      console.log('LOCATIONS', $scope.foursquareLocations)
-      console.log('updating search circle', $scope.avgLat, $scope.avgLon, $scope.radius);
+      // console.log('LOCATIONS', $scope.foursquareLocations)
+      // console.log('updating search circle', $scope.avgLat, $scope.avgLon, $scope.radius);
     })
   }
+
+
+  // Create some new functionality for Google Maps Custom Markers
+  function CustomMarker(latlng, map, args) {
+    this.latlng = latlng; 
+    this.args = args; 
+    this.setMap(map); 
+  }
+
+  CustomMarker.prototype = new google.maps.OverlayView();
+
+  CustomMarker.prototype.draw = function() {
+    
+    var self = this;
+    
+    var div = this.div;
+    
+    if (!div) {
+    
+      div = this.div = document.createElement('div');
+
+      div.className = 'marker-label';
+      div.style.position = 'absolute';
+      div.style.cursor = 'pointer';
+      div.style.width = '100px';
+      div.style.height = '20px';
+      
+      if (typeof(self.args.innerHTML) !== 'undefined') {
+        div.innerHTML = self.args.innerHTML;
+      }
+
+      if (typeof(self.args.marker_id) !== 'undefined') {
+        div.dataset.marker_id = self.args.marker_id;
+      }
+      
+      // Add an on click event to the name on the marker
+      google.maps.event.addDomListener(div, "click", function(event) {      
+        console.log(this.dataset.marker_id);
+        $scope.highlight = { selected: index };
+      });
+      
+      var panes = this.getPanes();
+      panes.overlayImage.appendChild(div);
+    }
+    
+    var point = this.getProjection().fromLatLngToDivPixel(this.latlng);
+    
+    if (point) {
+      div.style.left = (point.x) + 'px';
+      div.style.top = (point.y - 35) + 'px';
+    }
+  };
+
+  CustomMarker.prototype.remove = function() {
+    if (this.div) {
+      this.div.parentNode.removeChild(this.div);
+      this.div = null;
+    } 
+  };
+
+  CustomMarker.prototype.getPosition = function() {
+    return this.latlng; 
+  };
+
+
+
+
+
+
+
+
+
 
 
 });
