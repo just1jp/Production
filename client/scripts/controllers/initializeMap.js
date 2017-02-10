3f114f8d4fc01d4aa078cc348a473713e8bc1ea1
Kay Christensen <kaychristensen@gmail.com>
2017-02-09 16:37:24 -0800

    Merge

--- client/scripts/controllers/initializeMap.js
+++ client/scripts/controllers/initializeMap.js
@@ -8,19 +8,6 @@ angular.module('myApp').controller('initializeMap', function($rootScope, $scope,
   $scope.$on('user:updatedOrAdded', function(event, data) {
     $scope.userLocations[data[0]] = data[1];
     updateCenterPointAndRadius();
-<<<<<<< HEAD
-
-    databaseAndAuth.database.ref('/foursquare_results').once('value').then(function(snapshot) {
-      $scope.foursquareLocations = [];
-      for (key in snapshot.val()) {
-        $scope.foursquareLocations.push(snapshot.val()[key]);
-      }
-    });
-    
-    console.log('foursquare array', $scope.foursquareLocations);
-    console.log('foursquare location latitude', $scope.foursquareLocations[0].venue.location.lat);
-=======
->>>>>>> 321d383a843919cb26f18cb8c517e4b6416b6b80
     $scope.$apply();
   });
 
@@ -37,18 +24,6 @@ angular.module('myApp').controller('initializeMap', function($rootScope, $scope,
   NgMap.getMap().then(function(map) {
   });
 
-<<<<<<< HEAD
-  function renderLocationsonMap() {
-    databaseAndAuth.database.ref('/foursquare_results').once('value').then(function(snapshot) {
-      $scope.foursquareLocations = [];
-      for (key in snapshot.val()) {
-        $scope.foursquareLocations.push(snapshot.val()[key]);
-      }
-    });
-  }
-
-=======
->>>>>>> 321d383a843919cb26f18cb8c517e4b6416b6b80
   function updateCenterPointAndRadius() {
     coordinateCalc.getUserLocationData().then(function(coordinates) {
       circleData = coordinateCalc.calculateCircle(coordinates);
@@ -56,45 +31,9 @@ angular.module('myApp').controller('initializeMap', function($rootScope, $scope,
       $scope.avgLat = circleData.midpointLat;
       $scope.avgLon = circleData.midpointLon;
       $scope.radius = circleData.radius;
-<<<<<<< HEAD
-
-      databaseAndAuth.database.ref('/search_radius').set({
-        midpointLat: circleData.midpointLat,
-        midpointLon: circleData.midpointLon,
-        radius: circleData.radius
-      })
-
-      // renderLocationsonMap();
-
-=======
->>>>>>> 321d383a843919cb26f18cb8c517e4b6416b6b80
       console.log('updating search circle', $scope.avgLat, $scope.avgLon, $scope.radius);
     })
   }
 
 
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
